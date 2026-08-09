b06cff3d7153dd6179850b5eead78185069f20df
osiasnalaunan68-star <osiasnalaunan68@gmail.com>
2026-08-09 15:20:20 +0800

    Add Firebase Hosting, migrate to new Firebase project

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -4,12 +4,12 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBCAg768PHw5gy_yule6EpWsiAUQl9HgCE",
-  authDomain: "cuble-app-fe94d.firebaseapp.com",
-  projectId: "cuble-app-fe94d",
-  storageBucket: "cuble-app-fe94d.firebasestorage.app",
-  messagingSenderId: "415460175466",
-  appId: "1:415460175466:web:48cf182f6a51916ca06413",
+  apiKey: "AIzaSyARhpqrQM2WysnLDYNi7zHAm7gnsJt4X5I",
+  authDomain: "ra10863.firebaseapp.com",
+  projectId: "ra10863",
+  storageBucket: "ra10863.firebasestorage.app",
+  messagingSenderId: "160101866737",
+  appId: "1:160101866737:web:3fce8b9e40a9139b211d50",
 };
 
 export const app = initializeApp(firebaseConfig);
